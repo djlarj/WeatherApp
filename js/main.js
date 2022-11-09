e3c2ee6a8e4e1c130049a637acc8ffd8c870b380
x1-lreyes <33849865+x1-lreyes@users.noreply.github.com>
2022-11-09 02:00:33 -0800

    commented out console.log in js file

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ function getWeatherData () {
         
         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${API_KEY}`).then(res => res.json()).then(data => {
 
-        console.log(data);
+        // console.log(data);
         showWeatherData(data);
         });
     });
